1273fad2cfff9e727a5cc950495648550a7b9744
Lei Wang <leiw414@gmail.com>
2014-05-01 20:46:52 -0400

    Fix_View_Bugs
    Fix_View_Bugs

--- TaskManager/application/views/login.php
+++ TaskManager/application/views/login.php
@@ -1,4 +1,4 @@
-<div class="container">    
+    <div class="container">    
         <div id="loginbox" class=" margin-nav col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">                    
             <div class="panel panel-primary" >
                     <div class="panel-heading">
